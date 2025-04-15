64983e22d8815b03487168abf957a5d54ca067f5
CaparicaLeo <leo_caparica@icloud.com>
2025-04-15 16:04:22 -0300

    config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,25 +2,30 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
-import 'reflect-metadata';
-import '@shared/typeorm';
+import "reflect-metadata";
+import "@shared/typeorm";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): void => {
     if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
+      response.status(error.statusCode).json({
         status: "error",
         message: error.message,
       });
     }
-    return response.status(500).json({
-        status: "error",
-        message: 'Internal Server Error'
-    })
+    response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
   }
 );
 
